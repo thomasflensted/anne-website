dc452b895d29988369fe8645d5a1e47ceda542dd
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-05-15 13:17:20 +0200

    fixed broken link

--- src/components/TextBlockEnglish.tsx
+++ src/components/TextBlockEnglish.tsx
@@ -1,7 +1,7 @@
 const TextBlockEnglish = () => {
     return (
         <p className="leading-10 text-justify">
-            Hi, I'm Anne, and I'm a journalist. In the past couple of years at <a href="">Deadline</a>. I also write and proofread. I previously worked in politics, doing PR, speech writing, and communication. I'm open for work within journalism, copywriting and proofreading.
+            Hi, I'm Anne, and I'm a journalist. In the past couple of years at <a href="https://www.dr.dk/drtv/serie/deadline_7111" target="_blank">Deadline</a>. I also write and proofread. I previously worked in politics, doing PR, speech writing, and communication. I'm open for work within journalism, copywriting and proofreading.
         </p >
     )
 }
